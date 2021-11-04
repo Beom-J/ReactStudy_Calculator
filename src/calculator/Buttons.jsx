70440532e54cca82941364c8f6218fb248856ec7
Hyejin <beom00032@gmail.com>
2021-11-04 13:57:04 +0900

    일단 뭔가 계산은 되는데 오류투성이

--- src/calculator/Buttons.jsx
+++ src/calculator/Buttons.jsx
@@ -1,37 +1,75 @@
 import "./Buttons.css";
 
-function clickButton(event) {
-  console.log("click!: ", event.target.innerHTML);
-}
+function Buttons(props) {
+  const value = (event) => {
+    // Calculator 컴포넌트에 버튼 값 넘겨주기
+    const button = {
+      type: event.target.className,
+      value: event.target.innerHTML,
+    };
 
-function Buttons() {
+    props.click(button);
+  };
+
   return (
     <div className="button">
-      <button id="AC" onClick={clickButton}>
+      <button className="reset" id="AC" onClick={value}>
         AC
       </button>
-      <button onClick={clickButton}>+/-</button>
-      <button onClick={clickButton}>%</button>
-      <button onClick={clickButton}>/</button>
-      <button onClick={clickButton}>7</button>
-      <button onClick={clickButton}>8</button>
-      <button onClick={clickButton}>9</button>
-      <button onClick={clickButton}>X</button>
-      <button onClick={clickButton}>4</button>
-      <button onClick={clickButton}>5</button>
-      <button onClick={clickButton}>6</button>
-      <button onClick={clickButton}>-</button>
-      <button onClick={clickButton}>1</button>
-      <button onClick={clickButton}>2</button>
-      <button onClick={clickButton}>3</button>
-      <button onClick={clickButton}>+</button>
-      <button id="zero" onClick={clickButton}>
+      <button className="operator" onClick={value}>
+        +/-
+      </button>
+      <button className="operator" onClick={value}>
+        %
+      </button>
+      <button className="operator" onClick={value}>
+        /
+      </button>
+      <button className="number" onClick={value}>
+        7
+      </button>
+      <button className="number" onClick={value}>
+        8
+      </button>
+      <button className="number" onClick={value}>
+        9
+      </button>
+      <button className="operator" onClick={value}>
+        X
+      </button>
+      <button className="number" onClick={value}>
+        4
+      </button>
+      <button className="number" onClick={value}>
+        5
+      </button>
+      <button className="number" onClick={value}>
+        6
+      </button>
+      <button className="operator" onClick={value}>
+        -
+      </button>
+      <button className="number" onClick={value}>
+        1
+      </button>
+      <button className="number" onClick={value}>
+        2
+      </button>
+      <button className="number" onClick={value}>
+        3
+      </button>
+      <button className="operator" onClick={value}>
+        +
+      </button>
+      <button className="number" id="zero" onClick={value}>
         0
       </button>
-      <button id="point" onClick={clickButton}>
+      <button className="number" id="point" onClick={value}>
         .
       </button>
-      <button onClick={clickButton}>=</button>
+      <button className="equal" onClick={value}>
+        =
+      </button>
     </div>
   );
 }
--- src/calculator/Calculator.jsx
+++ src/calculator/Calculator.jsx
@@ -1,16 +1,72 @@
+import { useState } from "react";
 import Buttons from "./Buttons";
 import "./Calculator.css";
 
-let result = 0;
-
 function Calculator() {
+  let [inputValue, setinputValue] = useState(0);
+  let [
+    toBeCalculated = {
+      operator: "",
+      value: 0,
+    },
+    setToBeCalculated,
+  ] = useState(0);
+
+  const handleClick = (button) => {
+    if (button.type === "operator") {
+      setToBeCalculated({ operator: button.value, value: Number(inputValue) });
+      setinputValue((inputValue = 0));
+      console.log(toBeCalculated);
+    }
+    if (button.type === "number") {
+      setinputValue((inputValue += button.value));
+    }
+    if (button.type === "equal") {
+      // 계산
+      console.log("equal", inputValue);
+      const result = calculte(inputValue);
+      setinputValue((inputValue = result));
+    }
+    if (button.type === "reset") {
+      toBeCalculated = { perator: "", value: 0 };
+      setinputValue((inputValue = 0));
+    }
+  };
+
+  function calculte(value) {
+    let result = 0;
+    console.log("calculate", toBeCalculated.operator);
+
+    switch (toBeCalculated.operator) {
+      case "+/-":
+        break;
+      case "%":
+        result = Number(toBeCalculated.value) % Number(value);
+        break;
+      case "/":
+        result = Number(toBeCalculated.value) / Number(value);
+        break;
+      case "X":
+        result = Number(toBeCalculated.value) * Number(value);
+        break;
+      case "-":
+        result = Number(toBeCalculated.value) - Number(value);
+        break;
+      case "+":
+        result = toBeCalculated.value + Number(value);
+        break;
+    }
+
+    return result;
+  }
+
   return (
     // screen
     <div className="wrapper">
-      <div className="screen">result: {result}</div>
+      <div className="screen">{inputValue}</div>
       {/* button box */}
       <div className="button-box">
-        <Buttons />
+        <Buttons click={handleClick} />
       </div>
     </div>
   );
